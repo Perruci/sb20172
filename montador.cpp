2fa3ec3c1f38189ab45b71915cb24f4254943f3c
Tiago Gallo <tiagodecarvalhopereira@gmail.com>
2017-09-23 13:43:45 -0300

    Fiz algumas mudancas relacionadas a organizar o arquivo de uma maneira mais proxima a explicada no arquivo de documentacao. Nao consegui tempo ainda pra arrumar algumas varias coisas no codigo, entao fiz alguns comentarios pontuais nas coisas que eu estava trabalhando ainda. Dentro do switch eu inseri os nomes das funcoes que eu planejo criar para fazer cada um dos passos dos programas, mas ainda nao criei elas (observe que a saida de uma das funcoes e a entrada da funcao seguinte)

--- montador.cpp
+++ montador.cpp
@@ -2,8 +2,11 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <string.h>
 
 using namespace std;
+
+string pega_nome (char*);
 
 int main (int argc, char* argv[]){
 
@@ -17,21 +20,33 @@ int main (int argc, char* argv[]){
     //o argv[1] contem o argumento de operacao (ex:-m), como so queremos a letra sem o '-', estou pegando o char do endereco deslocado de 1 byte
     char operation = *(argv[1]+1); 
 
+    //Pega o nome do arquivo de entrada onde esta o codigo a ser analisado
+    string arq_entrada = argv[2];
+    
+    //Pega o nome do arquivo de saida que sera criado pelo nosso codigo, no entanto pega o nome do arquivo sem a extensao, pois podemos criar ele com ate 3 extensoes diferentes no programa
+    string arq_saida = pega_nome (argv[3]);
+
     switch (operation){
         //Operação de preprocessamento, coloca a estensão “.pre” no arquivo e somente 
         //avalia as diretivas EQU e IF.
         case 'p':
+            //pre-procesasmento (arq_entrada, arq_saida);
             cout << "Definir algo para fazer a operacao de preprocessamento\n";
             break;
 
         //Operação de processamento de macros, coloca a estensão “.mcr” no arquivo e somente avalia 
         //as diretivas EQU e IF e substitue as MACROS.
         case 'm':
+            //pre-procesasmento (arq_entrada, arq_saida);
+            //procesasmento (arq_saida);
             cout << "Definir algo para fazer a operacao de processamento\n";
             break;
 
         //Operação de motagem, coloca a estensão “.o” realiza a montagem de programa usando o PROCESSO DE PASSAGEM ÚNICA.
         case 'o':
+            //pre-procesasmento (arq_entrada, arq_saida);
+            //procesasmento (arq_saida);
+            //montador (arq_saida);
             cout << "Definir algo para fazer a operacao de montagem\n";
             break;
 
@@ -40,6 +55,9 @@ int main (int argc, char* argv[]){
             cout << "ERRO: O argumento " << argv[1] << " passado, nao condiz com nenhum modo de operacao\n";
             break;
     }
+
+    //Daqui pra baixo eu tinha feito para teste e para ver como ler o arquivo token por token, ainda quero tirar essa parte do main e colocar ela em cada uma das fucoes de pre-processamento,
+    //processamento e montagem (ainda nao tive tempo pra terminar).
 
     //Pega o nome do arquivo de entrada passado por argumento da linha de comando e abre esse arquivo para leitura usando a 
     //classe ifstream e chamando o arquivo de code.
@@ -72,10 +90,40 @@ int main (int argc, char* argv[]){
     return 0;
 }
 
-
+/*****************************************************************************
+Toda a declaracao da classe esta comentada pq tava dando um erro na compilacao e eu ainda nao tive tempo de ver  qual era o erro e arrumar
+//Declaracao da classe Token geral para todos os Tokens do programa
 class Token {
     public:
-        string nome;
+        string nome;  //field
 
-    
+        Token (string);  //construtor
+
+
+}
+
+//Declaracao do construtor da classe Token
+Token::Token(string a){
+    nome = a;
+}
+
+//Declarao dos metodos da classe Token
+
+
+/*****************************************************************************/
+
+//Funcao que analisa o nome do arquivo de saida passado como argumento de entrada e pega so a parte do nome antes da extensao (por exemplo: pega um triangulo.o e transforma em triangulo)
+string pega_nome (char* nome){
+    string aux = nome;
+    string aux2;
+    int tam = strlen (nome); //numero de caracteres da string nome
+
+    for (int i = 0; i < tam; i++){
+        if(nome[i] == '.'){
+            break;
+        }
+        aux2 += nome[i];
+    }
+
+    return aux2;
 }
