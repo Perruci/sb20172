57c66541fb9dc84f6c3043065969b40fe58b2833
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2017-10-07 15:49:06 -0300

    Deorecated function da classe token estão comentadas

--- src/token.cpp
+++ src/token.cpp
@@ -35,7 +35,7 @@ bool Token::isRotulo(std::string token, std::string linha, std::vector<Mnemonic>
     }
 
     //testa outros comandos que nao podem ser rotulos
-    if ((token == "equ") || (token == "if") || (token == "macro") || (token == "end") || 
+    if ((token == "equ") || (token == "if") || (token == "macro") || (token == "end") ||
         (token == "space") || (token == "const") || (isInteger(token))){
         return false;
     }
@@ -75,10 +75,68 @@ bool Token::isInteger(const std::string & aux)
 {
    if(aux.empty() || ((!isdigit(aux[0])) && (aux[0] != '-') && (aux[0] != '+'))){
     return false;
-   } 
+   }
 
    char* ptr;
    strtol(aux.c_str(), &ptr, 10);
 
    return (*ptr == 0) ;
 }
+
+/* Podem ser úteis */
+// // Função teste: extrair cada token do arquivo de entrada e escrevê-los de forma padrão na saída
+// void Montador::tokenizar()
+// {
+//     //string de suporte para capturar os tokens
+//     std::string aux;
+//     //Loop para ler todas as linhas do codigo aberto e "tokenizar" elas (o token leva o : em consideracao ainda)
+//     while (getline(this->fileText, aux)){
+//         char* ptr;  //ponteiro de suporte para o funcionamento do strlen dentro da funcao minuscula
+//         ptr = &(aux[0]);  //ponteiro apontando pro primeiro caracter da string
+//         aux = Montador::minuscula (ptr);
+//
+//
+//         std::stringstream lineStream (aux);
+//         Token token(aux);
+//
+//         while (lineStream >> token.nome){
+//             this->tokensList.push_back(token);
+//         }
+//     }
+// }
+//
+// /* Imprime Tokens guardades em tokensList */
+// void Montador::printTokens()
+// {
+//     if(this->tokensList.empty())
+//     {
+//         std::cout << "Token list empty" << '\n';
+//         return;
+//     }
+//     for(size_t i = 0; i < tokensList.size(); i++)
+//     {
+//         std::cout << tokensList[i].nome << " " << '\n';
+//     }
+//     std::cout << '\n';
+// }
+//
+// /* Escreve um arquivo de saida a partir de tokensList */
+// void Montador::writeTokensToOutput()
+// {
+//     for(size_t i = 0; i < tokensList.size(); i++)
+//     {
+//         this->fileOutput << tokensList[i].nome << '\n';
+//     }
+//     this->fileOutput << std::endl;
+// }
+//
+// //transforma toda a string em letras minusculas
+// std::string Montador::minuscula (char* aux){
+//     int tam = strlen (aux);
+//
+//     for (int i = 0; i < tam; i++){
+//         aux[i] = (char) std::tolower(aux[i]);
+//     }
+//
+//     return aux;
+// }
