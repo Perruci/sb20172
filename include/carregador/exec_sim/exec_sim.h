a25ec4c4603bd666367248acc3a431acc1938d21
Pedro Henrique S. Perruci <pedroperruci@hotmail.com>
2017-12-03 18:15:40 -0200

    Execution simulator now performs instructions

--- include/carregador/exec_sim/exec_sim.h
+++ include/carregador/exec_sim/exec_sim.h
@@ -14,11 +14,17 @@
 class ExecutionSimulator
 {
 private:
+    std::vector<MemorySpace> object_memory;
+    /* Register */
+    int reg_accum;
+    std::string last_instruction;
+    std::vector<int> arguments_list;
     /* Mnemonic */
     std::vector<Mnemonic> instructionList; // Instruction list for decoding
     int identifyInstruction(unsigned int);
     void printIntstructions();
     void loadInstructions(std::string tablePath = "tables/instructions.txt");
+    size_t processInstruction(std::vector<MemorySpace>, size_t);
 public:
     ExecutionSimulator();
     void simulate(std::vector<MemorySpace>);
--- src/exec_sim/exec_sim.cpp
+++ src/exec_sim/exec_sim.cpp
@@ -2,11 +2,13 @@
 
 ExecutionSimulator::ExecutionSimulator()
 {
+    this->reg_accum = 0;
     this->loadInstructions();
 }
 
-void ExecutionSimulator::simulate(std::vector<MemorySpace> object_memory)
+void ExecutionSimulator::simulate(std::vector<MemorySpace> obj_memory)
 {
+    this->object_memory = obj_memory;
     bool foundStop = false;
     unsigned int expected_args = 0;
     size_t program_counter = 0;
@@ -22,25 +24,128 @@ void ExecutionSimulator::simulate(std::vector<MemorySpace> object_memory)
             std::cout << value << "\t" << "Data" <<'\n';
         }
         // If the opcode is not identified, or an argument is expected, is an argument
-        else if ((idx == -1) | (expected_args > 0))
-        {
-            expected_args = expected_args - 1;
-            std::cout << value << "\t" << "Argument" <<'\n';
-        }
-        // If neither, must be an instruction
         else
         {
-            std::cout << value << "\t" << this->instructionList[idx].nome <<'\n';
-            expected_args = this->instructionList[idx].noperands;
-            // If instruction is stop, update found stop
-            if(this->instructionList[idx].nome == "stop")
+            if ((idx == -1) | (expected_args > 0))
             {
-                foundStop = true;
+                expected_args = expected_args - 1;
+                std::cout << value << "\t" << "Argument" <<'\n';
+                arguments_list.push_back(value);
             }
+            // If neither, must be an instruction
+            else
+            {
+                std::cout << value << "\t" << this->instructionList[idx].nome <<'\n';
+                this->last_instruction = this->instructionList[idx].nome;
+                expected_args = this->instructionList[idx].noperands;
+                // If instruction is stop, update found stop
+                if(this->instructionList[idx].nome == "stop")
+                {
+                    foundStop = true;
+                }
+            }
+            // If its the last expected argument, execute instruction
+            if(expected_args == 0)
+                program_counter = this->processInstruction(object_memory, program_counter);
         }
         program_counter++;
     }
 
+}
+
+/*
+    Executa instrução identificada por last_instruction com argumentos arguments_list
+ */
+size_t ExecutionSimulator::processInstruction(std::vector<MemorySpace> obejct_memory, size_t program_counter)
+{
+    size_t ret_address = program_counter;
+    if(this->last_instruction == "add")
+    {
+        std::cout << "Add " << this->reg_accum << " " << obejct_memory[arguments_list[0]].load() << '\n';
+        this->reg_accum += obejct_memory[arguments_list[0]].load();
+    }
+    if(this->last_instruction == "sub")
+    {
+        std::cout << "Sub " << this->reg_accum << " " << obejct_memory[arguments_list[0]].load() << '\n';
+        this->reg_accum -= obejct_memory[arguments_list[0]].load();
+    }
+
+    if(this->last_instruction == "mult")
+    {
+        std::cout << "Mult " << this->reg_accum << " " << obejct_memory[arguments_list[0]].load() << '\n';
+        this->reg_accum *= obejct_memory[arguments_list[0]].load();
+    }
+
+    if(this->last_instruction == "div")
+    {
+        std::cout << "Div " << this->reg_accum << " " << obejct_memory[arguments_list[0]].load() << '\n';
+        this->reg_accum /= obejct_memory[arguments_list[0]].load();
+    }
+
+    if(this->last_instruction == "jmp")
+    {
+        std::cout << "Jump " << this->arguments_list[0] << '\n';
+        ret_address = arguments_list[0];
+    }
+
+    if(this->last_instruction == "jmpn")
+    {
+        std::cout << "Jump if 0 > " << this->reg_accum << " to " << this->arguments_list[0] << '\n';
+        if(this->reg_accum < 0)
+            ret_address = this->arguments_list[0];
+    }
+
+    if(this->last_instruction == "jmpp")
+    {
+        std::cout << "Jump if 0 < " << this->reg_accum << " to " << this->arguments_list[0] << '\n';
+        if(this->reg_accum > 0)
+            ret_address = this->arguments_list[0];
+    }
+
+    if(this->last_instruction == "jmpz")
+    {
+        std::cout << "Jump if 0 == " << this->reg_accum << " to " << this->arguments_list[0] << '\n';
+        if(this->reg_accum == 0)
+            ret_address = this->arguments_list[0];
+    }
+
+    if(this->last_instruction == "copy")
+    {
+        std::cout << "Copy: [" << this->arguments_list[0] << "] [" << this->arguments_list[1] << "]" << '\n';
+        obejct_memory[arguments_list[0]].store(object_memory[arguments_list[1]].load());
+    }
+
+    if(this->last_instruction == "load")
+    {
+        std::cout << "Load: " << "Accum <- " << object_memory[arguments_list[0]].load() << '\n';
+        this->reg_accum = object_memory[arguments_list[0]].load();
+    }
+
+    if(this->last_instruction == "store")
+    {
+        std::cout << "Store: [" <<  this->arguments_list[0] << "] <- Accum" << '\n';
+        object_memory[this->arguments_list[0]].store(this->reg_accum);
+    }
+
+    if(this->last_instruction == "input")
+    {
+        std::cout << "[" << this->arguments_list[0] << "] <- Input" << '\n';
+        int input;
+        std::cin >> input;
+        object_memory[this->arguments_list[0]].store(input);
+    }
+
+    if(this->last_instruction == "output")
+    {
+        std::cout << "Output <- " << "[" << this->arguments_list[0] << "]" << '\n';
+        std::cout << this->object_memory[this->arguments_list[0]].load() << '\n';
+    }
+
+    if(this->last_instruction == "stop")
+        std::cout << "Stop! " << '\n';
+
+    arguments_list.clear();
+    return ret_address;
 }
 
 int ExecutionSimulator::identifyInstruction(unsigned int value)
